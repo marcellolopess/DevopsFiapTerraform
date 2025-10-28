8b3d0b4c575f8a27ce0c81a688b525099e81eba3
marcellolopess <marcellolopes.s@gmail.com>
2025-10-28 12:11:46 -0300

    Comment out cloud block in main.tf
    Comment out cloud organization configuration

--- main.tf
+++ main.tf
@@ -15,8 +15,8 @@ terraform {
   }
   required_version = ">= 1.1.0"
 
-  cloud {
-    organization = "DevopsFiap"
+  #cloud {
+    #organization = "DevopsFiap"
 
     workspaces {
       name = "gh-actions"
